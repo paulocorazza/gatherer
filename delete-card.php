a6174db02f6af15b193862be19b2b98abb8a3985
Carlos Encine <contato@carlosencine.com>
2021-11-02 18:56:59 -0300

    notas sonbre o delete

--- delete-card.php
+++ delete-card.php
@@ -12,14 +12,16 @@ if(!isset($_GET['id']) or !is_numeric($_GET['id'])){
   
   $card = Card::getCard($_GET['id']);
   
-  
+  // o que levaria um carda a não ser uma instancia de Card, sendo que o getCard é chamado diretamente do objeto Card?
   if(!$card instanceof Card){
     header('location: index.php');
     exit;
   }
-  
-if(isset($_POST['delete'])){
 
+// essa verificação aqui pode ser removida
+// e feita na linha 8 junto com a verificação de id
+if(isset($_POST['delete'])){
+    
   $card->deleteCard();
 
   header('location: index.php');
